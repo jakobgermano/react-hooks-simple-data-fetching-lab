0908d1a1da4b648738fde5d3d4a506f8def90cc8
jakob <jakob.germano@gmail.com>
2021-08-24 18:11:18 -0600

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+import React, { useEffect, useState } from 'react';
+
+
+function App() {
+  const [ data, setData ] = useState([])
+  const [ isLoaded, setIsLoaded ] = useState(false)
+
+  useEffect (() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+      .then(resp => resp.json())
+      .then(json => {
+          setData(json)
+          setIsLoaded(true)
+      })
+  }, [])
+  console.log(data) 
+
+  if (!isLoaded) return <p>Loading...</p>
+
+  const renderData = <img src={data.message} alt="A Random Dog"></img>
+  return(
+      <div>
+          {renderData}
+      </div>
+
+  )
+}
+
+    export default App;
+ 
+    
